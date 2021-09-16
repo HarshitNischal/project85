85654209800df7228a3af27f248f6a348ca126c8
HarshitNischal <71174516+HarshitNischal@users.noreply.github.com>
2021-09-16 17:06:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import  firebase from 'firebase'
+require('@firebase/firestore')
+
+// For Firebase JS SDK v7.20.0 and later, measurement
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCWpXBdODvzXD4hAN--UlBEWF0c7y9cfQ",
+  authDomain: "spectagramapp-fe506.firebaseapp.com",
+  projectId: "spectagramapp-fe506",
+  storageBucket: "spectagramapp-fe506.appspot.com",
+  messagingSenderId: "244692808369",
+  appId: "1:244692808369:web:c9ec55e83247dca66b6890"
+};
+  // Initialize Firebase
+
+
